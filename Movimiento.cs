23f0bf7b09c1c2e24cb9c0068282f51391971255
atorresmartinez <torres.martinez.a.123@gmail.com>
2020-01-08 13:23:18 -0800

    Subir scripts

---
+++ Movimiento.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+
+    public float Velocidad;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Mover()
+    {
+        Velocidad += 20;
+    }
+}
